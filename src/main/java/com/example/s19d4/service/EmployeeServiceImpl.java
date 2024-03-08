4ea332cfca79b3b7db39f59ab6c9537d47fce66a
skaimarvas <skaimarvas@gmail.com>
2024-03-08 22:57:29 +0300

    EmployeeServiceImpl was created

---
+++ src/main/java/com/example/s19d4/service/EmployeeServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.s19d4.service;
+
+import com.example.s19d4.entity.Employee;
+import com.example.s19d4.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(long id) {
+        Optional<Employee> foundEmployee = employeeRepository.findById(id);
+        if(foundEmployee.isPresent()){
+            return foundEmployee.get();
+        }
+        return null;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee remove(long id) {
+        Employee employee = findById(id);
+
+        if(employee != null){
+            employeeRepository.delete(employee);
+
+            return employee;
+        }
+        return null;
+    }
+}
