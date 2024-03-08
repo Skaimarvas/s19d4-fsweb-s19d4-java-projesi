69f7bf64c60175e8f9d3a49f19d09e7a8db86e12
skaimarvas <skaimarvas@gmail.com>
2024-03-08 22:42:47 +0300

    Employee entity was created

---
+++ src/main/java/com/example/s19d4/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.example.s19d4.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "employee", schema = "s19d4")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "salary")
+    private int salary;
+
+}
